a0ad908adf53d39bf2db02893b43482aff48e713
Álvaro Cozano Pérez <148540726+alvaro-cozano@users.noreply.github.com>
2025-05-22 20:03:33 +0200

    Despliegue

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:8080',
@@ -14,4 +16,5 @@ export default defineConfig({
       },
     },
   },
+  base: './',
 })
