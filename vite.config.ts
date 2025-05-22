cdad5439e65576d27cf6adfbc87fa9d72c9c758b
Álvaro Cozano Pérez <148540726+alvaro-cozano@users.noreply.github.com>
2025-05-22 20:08:45 +0200

    Taskor MVP

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
   server: {
     proxy: {
       '/api': 'http://localhost:8080',
@@ -16,5 +13,4 @@ export default defineConfig({
       },
     },
   },
-  base: './',
 })
