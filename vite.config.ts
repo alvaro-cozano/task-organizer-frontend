7e2a0e837a0a7d52f706bde66c2adbba7f79b2eb
alvaro-cozano <alvarocozano@gmail.com>
2025-05-26 14:48:57 +0200

    Modificaciones para despliegue

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8080',
-      '/ws': {
-        target: 'http://localhost:8080',
-        ws: true,
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': env.VITE_API_URL,
+        '/ws': {
+          target: env.VITE_API_URL,
+          ws: true,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  }
 })
